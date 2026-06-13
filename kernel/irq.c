f2f749849f67a308fde39880a6ab90fcd67bd533
jrf-g <jrfaragalla@gmail.com>
2026-06-13 14:21:15 -0700

    Enhance IRQ handler with PIT tick functionality
    Added PIT handling to the IRQ handler and removed temporary IRQ print statements.

--- kernel/irq.c
+++ kernel/irq.c
@@ -1,17 +1,18 @@
 #include "irq.h"
 #include "pic.h"
 #include "kprint.h"
+#include "pit.h"
+#def TICK
 
 void irq_handler_c(struct irq_regs* r) {
     int irq = r->int_no - 32;
 
-    // TEMP: print IRQ number
-    kprint("IRQ ");
-    char buf[4];
-    buf[0] = '0' + irq;
-    buf[1] = '\n';
-    buf[2] = 0;
-    kprint(buf);
+    if (irq == 0) {
+        pit_tick();
+        #ifdef TICK
+        if ((pit_ticks() % 100) == 0) kprint("tick\n");
+        #endif
+    }
 
     pic_send_eoi(irq);
 }
