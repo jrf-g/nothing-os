7d24025c0efcbcf736108a583bde87e701b9a6d8
jrf-g <jrfaragalla@gmail.com>
2026-06-13 14:01:39 -0700

    Add IRQ handler structure and function prototype

---
+++ kernel/irq.c
@@ -0,0 +1,11 @@
+#pragma once
+#include <stdint.h>
+
+struct irq_regs {
+    uint32_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, esp_dummy, ebx, edx, ecx, eax;
+    uint32_t int_no, err_code;
+    uint32_t eip, cs, eflags, useresp, ss;
+};
+
+void irq_handler_c(struct irq_regs* r);
