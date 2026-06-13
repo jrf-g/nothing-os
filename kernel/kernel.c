67064116f19347278c28dd02bf6d731d5b38693c
jrf-g <jrfaragalla@gmail.com>
2026-06-13 15:10:42 -0700

    Refactor kernel_main to support multiboot info

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,24 +1,38 @@
-#include "gdt.h"
+#include <stdint.h>
 #include "kprint.h"
+#include "gdt.h"
 #include "idt.h"
 #include "pic.h"
 #include "pit.h"
-#def SPEED 100
+#include "multiboot.h"
+#include "mm.h"
 
-void kernel_main(void) {
-    gdt_init();
-    kprint("GDT loaded.\n");
-
+void kernel_main(uint32_t magic, uint32_t mb_info_addr) {
     kclear();
-    kprint("Booting kernel...\n");
+    kprint("Kernel start.\n");
 
+    if (magic != MULTIBOOT_MAGIC) {
+        kprint("Bad multiboot magic.\n");
+        for (;;) {}
+    }
+
+    gdt_init();
     pic_remap();
     idt_init();
-    pit_init(SPEED);
+    pit_init(100);
     __asm__ volatile("sti");
 
-    kprint("Interrupts loaded.\n");
+    mm_init(mb_info_addr);
+
+    kprint("Testing kmalloc...\n");
+    void* a = kmalloc(128);
+    void* b = kmalloc(256);
+    kfree(a);
+    void* c = kmalloc(64);
+
+    kprint("Memory system online.\n");
 
     for (;;) {
+        // idle
     }
 }
