d259a9c954031be1822a37a257d24b3b734c852c
jrf-g <jrfaragalla@gmail.com>
2026-06-13 14:58:14 -0700

    Add multiboot header definitions

---
+++ kernel/multiboot.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdint.h>
+
+#define MULTIBOOT_MAGIC 0x2BADB002
+
+struct multiboot_mmap_entry {
+    uint32_t size;
+    uint64_t addr;
+    uint64_t len;
+    uint32_t type;
+} __attribute__((packed));
+
+struct multiboot_info {
+    uint32_t flags;
+    uint32_t mem_lower;
+    uint32_t mem_upper;
+    uint32_t boot_device;
+    uint32_t cmdline;
+    uint32_t mods_count;
+    uint32_t mods_addr;
+    uint32_t syms[4];
+    uint32_t mmap_length;
+    uint32_t mmap_addr;
+    // (we don't need the rest for now)
+} __attribute__((packed));
