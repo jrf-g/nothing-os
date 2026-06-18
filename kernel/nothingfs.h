95bf8ab3c25de4a6659bc74aa8c0dbea7bba5c15
jrf-g <jrfaragalla@gmail.com>
2026-06-17 17:59:54 -0700

    Add nfs_remove function declaration

--- kernel/nothingfs.h
+++ kernel/nothingfs.h
@@ -16,6 +16,7 @@ int      nfs_create(const char* path);
 int      nfs_write(const char* path, const void* data, uint32_t len);
 int      nfs_read(const char* path, void* out, uint32_t max);
 void     nfs_list(const char* path);
+int nfs_remove(const char* path);
 
 uint32_t nfs_serialize(uint8_t* buf, uint32_t max);
 uint32_t nfs_deserialize(const uint8_t* buf, uint32_t len);
