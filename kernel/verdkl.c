92905f6669cef165cba8a467cf60b08f0254595e
jrf-g <jrfaragalla@gmail.com>
2026-07-02 15:14:50 -0700

    Modify badlib function to change halt behavior

--- kernel/verdkl.c
+++ kernel/verdkl.c
@@ -1,7 +1,6 @@
 #include "powerctl.h"
 #include "kprint.c"
 void badlib() {
-  kprint("BAD MODULES HAVE BEEN LOADED; UNLOADING THEM NOW. THE SYSTEM WILL LOCK. PRESS ANY KET TO UNLOCK");
-  __asm__ volatile("hlt");
-  reboot();
+  kprint("BAD MODULES HAVE BEEN LOADED; UNLOADING THEM NOW. THE SYSTEM WILL LOCK.");
+  __asm__ volatile("int $32");
 }
