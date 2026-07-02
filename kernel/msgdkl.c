20901412f97f6cce9922e11558a6fa07bc385ba1
jrf-g <jrfaragalla@gmail.com>
2026-07-02 09:57:44 -0700

    Enhance msgdkl with security checks and verification
    Added verification function and modified print behavior based on security status.

--- kernel/msgdkl.c
+++ kernel/msgdkl.c
@@ -1,4 +1,17 @@
-#include  "msgdkl.h"
+#include "msgdkl.h"
+#include "verdkl.h"
+#include "powerctl.h"
+int secure = 0;
 void msgdkl_print() {
-  kprint(DKLTXT)
+  if (!secure) {
+    shutdown();
+  }
+  kprint(DKLTXT);
+}
+void msgdkl_verify() {
+  if (MSGDKL_IMP_KEY == GB_IMP_KEY) {
+    int secure = 1;
+  } else {
+    int secure = 0;
+  }
 }
