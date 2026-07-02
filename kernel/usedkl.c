9a622288b46752c7655e5fb81750b25302fa780e
jrf-g <jrfaragalla@gmail.com>
2026-07-02 11:23:50 -0700

    clarify comments

--- kernel/usedkl.c
+++ kernel/usedkl.c
@@ -1,9 +1,9 @@
 // dkl modules are the NothingOS version of a DLL
-// add dkl imports below
+// add dkl inlcudes below
 #include "msgdkl.h"
 // just add the dkl call in the initdkl function
 // the functions of all dkls should start with the name of the dkl
 void initdkl() {
-  msgdkl_verify();
-  msgdkl_print();
+  msgdkl_verify(); // activate msgdkl
+  msgdkl_print(); // use msgdkl
 }
