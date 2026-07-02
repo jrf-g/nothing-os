f615194ab79ec3763b71b06395e53fa96eb89278
jrf-g <jrfaragalla@gmail.com>
2026-07-02 14:04:48 -0700

    Update include statement for msgdkl header

--- kernel/usedkl.c
+++ kernel/usedkl.c
@@ -1,6 +1,6 @@
 // dkl modules are the NothingOS version of a DLL
 // add dkl inlcudes below
-#include "msgdkl.h"
+#include "msg.dkl"
 // just add the dkl call in the initdkl function
 // the functions of all dkls should start with the name of the dkl
 void initdkl() {
