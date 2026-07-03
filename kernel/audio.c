d6f2f9ddbf3cd3e98ebc9542013a7641836fb383
jrf-g <jrfaragalla@gmail.com>
2026-07-02 18:34:50 -0700

    Add beep function for speaker control
    Implement beep function to control speaker frequency and duration.

---
+++ kernel/audio.c
@@ -0,0 +1,21 @@
+#include "time.c"
+void beep(unsigned int freq, unsigned int duration)
+{
+    unsigned int div = 1193180 / freq;
+
+    /* Program PIT channel 2 */
+    outb(0x43, 0xB6);
+    outb(0x42, (unsigned char)(div & 0xFF));
+    outb(0x42, (unsigned char)((div >> 8) & 0xFF));
+
+    /* Enable speaker (bits 0 and 1) */
+    unsigned char tmp = inb(0x61);
+    outb(0x61, tmp | 0x03);
+
+    /* Wait */
+    delay(duration);
+
+    /* Disable speaker (clear bit 1) */
+    tmp = inb(0x61);
+    outb(0x61, tmp & ~0x02);
+}
