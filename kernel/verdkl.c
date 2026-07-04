3064ab8bd910b061fb3e3a09bf002b2c7e48691e
jrf-g <jrfaragalla@gmail.com>
2026-07-03 18:14:55 -0700

    Fix inline assembly syntax for interrupt

--- kernel/verdkl.c
+++ kernel/verdkl.c
@@ -2,5 +2,5 @@
 #include "kprint.c"
 void badlib() {
   kprint("BAD MODULES HAVE BEEN LOADED; UNLOADING THEM NOW. THE SYSTEM WILL LOCK.");
-  __asm__ volatile("int 32");
+  __asm__ volatile("int $32");
 }
