991e5159be922d840113cafc01acfde0079cf2da
jrf-g <jrfaragalla@gmail.com>
2026-07-21 12:38:50 -0700

    Improve reboot and shutdown functions
    Enhanced reboot and shutdown functions for better compatibility across different PC eras. Added fallback mechanisms for rebooting and ensured hardoff is called during shutdown.

--- kernel/powerctl.c
+++ kernel/powerctl.c
@@ -1,15 +1,16 @@
 #include powerctl.h
 void reboot() {
-  outw(0xB004, 0x0006); // ACPI
-  // Keyboard Reset Method
-  while (inb(0x64) & 0x02);
-  outb(0x64, 0xFE);
+  outw(0xB004, 0x0006); // reboot on 1990s-2000s PCs and QEMU
+  outb(0xCF9, 0x06); // reboot on mid-era PCs using chipset hacks
+  outb(0x64, 0xFE); // reboot everywhere fallback
 }
 void shutdown(uint16_t pm1a_cnt_blk, uint16_t slp_typa) {
-  outw(pm1a_cnt_blk, slp_typa | (1 << 13));
   outw(0x604, 0x2000);
+  outw(pm1a_cnt_blk, slp_typa | (1 << 13));
+  hardoff(); // if we cant shut down, then halt
+  reboot(); // if we cant halt, then reboot
 }
 extern hardoffasm
 void hardoff() {
-  hardoffasm();
+  hardoffasm(); // invoke assembly shutdown routine
 }
