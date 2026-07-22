ca5f2c8cd64a57c7b571ab9de7e367b290832ac3
jrf-g <jrfaragalla@gmail.com>
2026-07-22 14:07:06 -0700

    SOMETHING V2.0

--- kernel/corgi.c
+++ kernel/corgi.c
@@ -1,6 +1,10 @@
 #include mm.c
 void puppy_go_brrr_glitchy() {
   void* shiba_inu = kmalloc(3072);
-  safealloc(12);
-  free(shiba_inu);
+  if (shiba_inu) {
+    safealloc(12);
+    free(shiba_inu);
+  } else {
+    oom();
+  }
 }
