e242bca382ece14aa7a7d93f6c0c7882f0df0411
jrf-g <jrfaragalla@gmail.com>
2026-07-29 19:21:37 -0700

    Add inline assembly functions and update shutdown logic
    Added inline assembly functions for outputting bytes and words to specified ports. Updated shutdown function to include a new output operation.

--- kernel/powerctl.c
+++ kernel/powerctl.c
@@ -1,4 +1,13 @@
 #include powerctl.h
+static inline void outb(uint16_t port, uint8_t value) {
+    __asm__ volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+static inline void outw(uint16_t port, uint16_t value) {
+    __asm__ volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+void acpi_initialize() {
+  outb(0xB2, 0xA0);
+}
 void reboot() {
   outw(0xB004, 0x0006); // reboot on 1990s-2000s PCs and QEMU
   outb(0xCF9, 0x06); // reboot on mid-era PCs using chipset hacks
@@ -6,6 +15,7 @@ void reboot() {
 }
 void shutdown(uint16_t pm1a_cnt_blk, uint16_t slp_typa) {
   outw(0x604, 0x2000);
+  outw(0x404, 0x0x7);
   outw(pm1a_cnt_blk, slp_typa | (1 << 13));
   hardoff(); // if we cant shut down, then halt
   reboot(); // if we cant halt, then reboot
