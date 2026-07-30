081e3b99e6d342722087bfa45c265c222d7a884b
jrf-g <jrfaragalla@gmail.com>
2026-07-30 15:49:06 -0700

    Refactor safe mode handling in kernel_main
    Refactor kernel_main to use preprocessor directives for safe mode instead of a variable.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,4 @@
+// vim: syntax=c
 #include <stdint.h>
 #include "kprint.h"
 #include "sh.h"
@@ -12,8 +13,6 @@
 #include "usedkl.h"
 #include "startmem.h"
 #include "audio.h"
-#define SAFEMODE = 0
-uint32_t fancy = 0;
 typedef void* MemAddr;
 typedef unsigned char* BytesPointer;
 void chime() {
@@ -37,10 +36,9 @@ void memtest(void* ptr, unsigned int size)
 void kernel_main(uint32_t magic, uint32_t mb_info_addr) {
     kclear();
     kprint("Kernel start.\n");
-    fancy = !SAFEMODE
-    if (!fancy) {
-        kprint("SAFE MODE ENABLED.");
-    }
+    #ifdef SAFEMODE
+    kprint("SAFE MODE ENABLED.");
+    #endif
     if (magic != MULTIBOOT_MAGIC) {
         beep(300, 100);
         beep(500, 100);
@@ -51,13 +49,15 @@ void kernel_main(uint32_t magic, uint32_t mb_info_addr) {
     }
 
     gdt_init();
+
     pic_remap();
     idt_init();
-    if (fancy)  {
+    #ifdef SAFEMODE
         autoclk();
-    } else {
+    #endif
+    #ifndef SAFEMODE
         pit_init(FREQ);
-    }
+    #endif
     keyboard_init();
     __asm__ volatile("sti");
     if (fancy) {
@@ -67,7 +67,6 @@ void kernel_main(uint32_t magic, uint32_t mb_info_addr) {
     acpi_initialize();
     nfs_init();
     nfs_mkdir("/default");
-
     kprint("Testing kmalloc...\n");
     MemAddr a = kmalloc(STARTMEMINT);
     kfree(a);
